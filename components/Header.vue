c8a3bd6072a7e83b081b46ea5e97b761a6d25c3d
ahmadsan <ahmadsan4p@gmail.com>
2023-04-17 18:16:32 +0800

    revertion

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
     <section id="topbar" class="topbar d-flex align-items-center">
       <b-container
         class="d-flex justify-content-center justify-content-md-between"
-        >
+      >
         <div class="contact-info d-flex align-items-center">
           <div>
             <i class="bi bi-instagram"></i>
@@ -14,12 +14,12 @@
               >alihizaam.id</a
             >
           </div>
-              <div style="margin: 0 15px; text-decoration: none">
-                <i class="bi bi-whatsapp"></i>
-                <a style="color: black" href="https://wa.me/6282349756601"
-                  >0823-4975-6601</a
-                >
-              </div>
+          <div style="margin: 0 15px; text-decoration: none">
+            <i class="bi bi-whatsapp"></i>
+            <a style="color: white" href="https://wa.me/6282349756601"
+              >0823-4975-6601</a
+            >
+          </div>
         </div>
       </b-container>
     </section>
@@ -29,10 +29,10 @@
       <b-container
         fluid
         class="d-flex align-items-center justify-content-between"
-        >
+      >
         <a href="index.html" class="logo d-flex align-items-center">
           <!-- Uncomment the line below if you also wish to use an image logo -->
-        <img src="assets/img/alihizaam.png" alt="" />
+          <img src="assets/img/alihizaam.png" alt="" />
         </a>
         <nav id="navbar" class="navbar">
           <ul>
